107ffcd8c1512ae00078dea1c3e5d42680070de2
ibanMendez <Imenor7@yahoo.es>
2015-11-07 21:42:56 +0100

    Canvis confirmats

---
+++ src/ibanMendez/PreguntaDos.java
@@ -0,0 +1,20 @@
+package ibanMendez;
+
+public class PreguntaDos {
+
+	private String atributo;
+
+	public String getAtributo() {
+		return atributo;
+	}
+
+	public void setAtributo(String atributo) {
+		this.atributo = atributo;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
